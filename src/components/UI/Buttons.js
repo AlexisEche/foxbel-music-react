392139eb8705c9e495f0a9bcd99695582300b00a
AlexisEche <alexis.eche58@gmail.com>
2021-03-23 11:16:14 -0500

    Add the button component styled

--- src/components/UI/Buttons.js
+++ src/components/UI/Buttons.js
@@ -1,13 +1,13 @@
 function PrimaryButton({children}){
   return(
-    <button className="w-28	bg-red-400 h-8 rounded-2xl text-white text-sm focus:outline-none hover:bg-red-500">
+    <button className="w-28	bg-red-600 h-8 rounded-2xl text-white text-sm focus:outline-none hover:bg-red-700">
       {children}
     </button>
   )
 }
 function SecondaryButton({children}){
   return(
-    <button className="w-28	bg-none h-8 rounded-2xl text-red-400 text-sm border border-red-400 focus:outline-none ">
+    <button className="w-28	bg-none h-8 rounded-2xl text-red-600 text-sm border-2 border-red-600 focus:outline-none hover:text-white hover:bg-red-600">
       {children}
     </button>
   )
