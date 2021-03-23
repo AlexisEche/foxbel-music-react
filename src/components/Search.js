b4da2ada98eb660216fdbaebecd59eda49dc857a
AlexisEche <alexis.eche58@gmail.com>
2021-03-23 11:15:00 -0500

    Add the search component styled

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,12 @@
+import Icon from "./UI/Icons";
+
 function Search(){
   return(
-    <>
-    Search
-    </>
+      <div className="border-gray-400 rounded-3xl w-6/12 border pt-1.5 pr-4  pb-2 pl-4 flex justigy-between items-center">
+        <input type="text" placeholder="Buscar" className="w-full focus:outline-none"/>
+        <Icon type="search" color="#BDBDBD" size="18px"></Icon>
+      </div>
+
   )
 }
 
