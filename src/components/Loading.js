7964ef927d466c5d461ffad9ffcc24b865d29691
AlexisEche <alexis.eche58@gmail.com>
2021-03-24 11:02:31 -0500

    Add Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,12 @@
 function Loading(){
   return(
-    <div>
-      Loading
-    </div>
+    <span className="pb-32">
+      <div className="loader bg-red-400 w-32 p-5 rounded-full flex space-x-3">
+        <div className="w-5 h-5 bg-white rounded-full animate-bounce"/>
+        <div className="w-5 h-5 bg-white rounded-full animate-bounce"/>
+        <div className="w-5 h-5 bg-white rounded-full animate-bounce"/>
+      </div>
+    </span>
   )
 }
 export default Loading
